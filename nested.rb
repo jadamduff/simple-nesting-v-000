5aa7e87ef9f4971a26766caa0f12e11842ca0b6d
Adam Duff <j.adam.duff@gmail.com>
2018-08-14 22:10:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -70,10 +70,6 @@ def adding_matz
 
 	programmer_hash =
  		{
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-       },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -88,7 +84,8 @@ def adding_matz
         }
      }
 
-
+     programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash
 end
 
 def changing_alan
