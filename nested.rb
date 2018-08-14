348de27fe13cb2d3217a95f91fbc832d5e914bbb
Adam Duff <j.adam.duff@gmail.com>
2018-08-14 21:58:37 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -92,7 +92,6 @@ def adding_matz
 end
 
 def changing_alan
-  alans_new_info = "GUI"
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -100,7 +99,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => alans_new_info,
+          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -109,7 +108,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-
+     alans_new_info = "GUI"
+     programmer_hash[:alans_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
