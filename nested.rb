ac813486e6824945bb7cf9cce4bcb774bdb6532c
Adam Duff <j.adam.duff@gmail.com>
2018-08-14 22:06:54 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,30 +109,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-     programmer_hash[:alans_kay][:known_for] = alans_new_info
-     programmer_hash
-end
-
-def changing_alan
-  programmer_hash =
-    {
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-     #change what Alan Kay is :known_for the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-     
-     programmer_hash[:alans_kay][:known_for] = alans_new_info
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      programmer_hash
 end
 
